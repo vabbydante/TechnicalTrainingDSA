2fe1f45be41e814b42f8adf056a1f2fcd43bf4d6
vaibhav <rohitvaibhavgupta@gmail.com>
2022-07-17 11:42:27 +0530

    Initial commit

---
+++ src/com/vaibhav/coding/HelloWorld.java
@@ -0,0 +1,8 @@
+package com.vaibhav.coding;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("just another text!");
+    }
+}
