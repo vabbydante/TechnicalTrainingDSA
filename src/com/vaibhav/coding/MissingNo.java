cfdc78286f7eac06efc0d7513bf8e00561c18f7e
vabbydante <rohitvaibhavgupta@gmail.com>
2022-07-22 10:17:10 +0530

    Initial Commit

---
+++ src/com/vaibhav/coding/MissingNo.java
@@ -0,0 +1,34 @@
+package com.vaibhav.coding;
+
+import java.util.Scanner;
+
+// Find the missing number in a given array of 'n' natural numbers.
+public class MissingNo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // taking inputs from the user :
+        System.out.println("Enter the number of elements : ");
+        int size = sc.nextInt();
+        int[] arr = new int[size];
+        for (int k = 0; k<size; k++){
+            arr[k] = sc.nextInt();
+        }
+
+        // setting the variables :
+        int actual = 0;
+        int sum = 0;
+        for (int j : arr) {
+            sum = sum + j;
+        }
+
+        // taking the first and last element from the user's array :
+        int first;
+        int last = arr[arr.length-1];
+
+        for(first = arr[0]; first<=last; first++){
+            actual = actual+first;
+        }
+        int result = actual-sum;
+        System.out.println("The missing element is : " + result);
+    }
+}
